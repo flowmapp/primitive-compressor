31250b5b9f9cbbd35e7c705a8b2f301b9538143f
Igor Tatarintsev <ietatarin@gmail.com>
2023-10-30 09:43:51 +0100

    Delete decompress wireframe primitive export for node.js

--- node.js
+++ node.js
@@ -1,5 +1,4 @@
 module.exports = {
   compressWireframePrimitive: wireframePrimitive => require('./compress')('wireframePrimitive', wireframePrimitive),
-  decompressWireframePrimitive: wireframePrimitive => require('./decompress')('wireframePrimitive', wireframePrimitive),
   compressUserflowBlock: userflowBlock => require('./compress')('userflowBlock', userflowBlock),
 }

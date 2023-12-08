f03c3f26e7fed9c31a9c0bbf185b234d24ce66c0
Igor Tatarintsev <ietatarin@gmail.com>
2023-12-08 21:21:07 +0600

    Add palette to colors

--- const/wireframePrimitive/valueMapping/color.js
+++ const/wireframePrimitive/valueMapping/color.js
@@ -2,4 +2,14 @@ module.exports = require('../../../utils/makeMapping')([
   '#1f1f1f', //
   '#999',
   '#e0e0e0',
+  '#6696ff',
+  '#c2c2c2',
+  '#36eb8d',
+  '#ff7070',
+  '#ffa334',
+  '#f5cc00',
+  '#3af3ff',
+  '#9b70ff',
+  '#ff99ff',
+  '#adadad',
 ])
--- dist/node.js
+++ dist/node.js
@@ -3889,7 +3889,18 @@ var require_backgroundColor = __commonJS({
     module2.exports = require_makeMapping()([
       "black",
       //
-      "white"
+      "white",
+      "#1f1f1f",
+      "#6696ff",
+      "#c2c2c2",
+      "#36eb8d",
+      "#ff7070",
+      "#ffa334",
+      "#f5cc00",
+      "#3af3ff",
+      "#9b70ff",
+      "#ff99ff",
+      "#adadad"
     ]);
   }
 });
@@ -3901,7 +3912,17 @@ var require_color = __commonJS({
       "#1f1f1f",
       //
       "#999",
-      "#e0e0e0"
+      "#e0e0e0",
+      "#6696ff",
+      "#c2c2c2",
+      "#36eb8d",
+      "#ff7070",
+      "#ffa334",
+      "#f5cc00",
+      "#3af3ff",
+      "#9b70ff",
+      "#ff99ff",
+      "#adadad"
     ]);
   }
 });
